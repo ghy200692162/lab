25383d0f7dc9f58bc0f625094329d4e7b6a730fa
ghy <ghy200692162@126.com>
2012-09-01 11:38:53 +0800

    modify url

--- activity/urls.py
+++ activity/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 ACTIVITY_URL=r'(?P<slug>[-\w]+)/'
 
 urlpatterns = patterns('activity.views',
-        url(r'^create/$',                 'activity_create',     name='activity_create'),
-        url(r'^activity_list/$',                        'activity_list',       name='activity_list'),
+        url(r'^create/$',                       'activity_create',     name='activity_create'),
+        url(r'^activity_list/$',                'activity_list',       name='activity_list'),
         url(r'^%sdetail$' % ACTIVITY_URL,       'activity_detail',      name='activity_detail'),
         )
